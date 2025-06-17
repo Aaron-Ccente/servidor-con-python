3a67b2e2740a11dfa1fb7cf4c6739a3e6e43484d
Aaron-Ccente <aronccente@gmail.com>
2025-06-16 23:37:02 -0500

    metodos referenciales añadidos

--- src/routes/example_routes.py
+++ src/routes/example_routes.py
@@ -4,3 +4,11 @@ from . import api_bp
 @api_bp.route('/hello', methods=['GET'])
 def hello():
     return jsonify({"message": "¡Hola desde el backend Python!"})
+
+@api_bp.route('/world', methods=['GET'])
+def world():
+    return jsonify({"message": "world"})
+
+@api_bp.route('/amigo', methods=['GET'])
+def amigo():
+    return jsonify({"message": "amigo"})
